eca32166dfaf8ee36eb6590fafdda5124d03b156
tavogg77 <gguerravelasquez@byupathway.edu>
2025-09-20 15:15:30 -0600

    Excercise 3 completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+
+        string keep = "yes";
+        while (keep == "yes")
+        {
+            int number = randomGenerator.Next(1, 11);
+            int guess = -1;
+            int countg = 0;
+            while (guess != number)
+            {
+                Console.Write("What is your guess? ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                countg++;
+
+                if (guess < number)
+                    Console.WriteLine("Higher");
+                else if (guess > number)
+                    Console.WriteLine("Lower");
+                else
+                    Console.WriteLine("You guessed it!");
+
+            }
+            Console.WriteLine($"It took you {countg} guesses");
+            Console.WriteLine();
+            Console.WriteLine("Would you like to play again (yes/no) ");
+            keep = Console.ReadLine();
+        }
+        Console.WriteLine("Thank you for playing. Goodbye.");
     }
 }
