112f1571279518aa2a5c197dee8e5f39ce1637ec
tavogg77 <gguerravelasquez@byupathway.edu>
2025-11-06 21:33:01 -0600

    Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+        
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        string additional;
+
+        
+        if (grade < 90 && grade > 60 && grade % 10 >= 7)
+        {
+            additional = "+";
+        }
+        else if (grade < 90 && grade > 60 && grade % 10 <= 3)
+        {
+            additional = "-";
+        }
+        else
+        {
+            additional = "";
+        }
+
+
+        Console.WriteLine($"Your grade is {letter}{additional}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congratulation, you passed!");
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, you did not pass, keep working for next chance");
+        }
     }
 }
