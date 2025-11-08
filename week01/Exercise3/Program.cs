9cdb941823588a9725c48599fcb0eddfd860b15d
tavogg77 <gguerravelasquez@byupathway.edu>
2025-11-07 21:31:26 -0600

    Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        string play = "";
+        do
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 11);
+
+            int guess;
+            int counter = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                counter++;
+
+
+            } while (guess != number);
+
+            Console.WriteLine("You guess it!");
+            Console.WriteLine($"Attempts: {counter}");
+            Console.WriteLine("Do you want to play again?");
+            play = Console.ReadLine();
+        } while (play == "yes");
+
+        
     }
 }
