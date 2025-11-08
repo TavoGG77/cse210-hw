57592ddc0e96b21cdaa7082f280501faba4e74d2
tavogg77 <gguerravelasquez@byupathway.edu>
2025-11-07 22:42:44 -0600

    Excercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,56 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int option = -1;
+        int counter = 0;
+        int sum = 0;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            option = int.Parse(Console.ReadLine());
+
+            if (option != 0)
+            {
+                numbers.Add(option);
+            }
+
+            sum += option;
+            counter++;
+            
+        } while (option != 0);
+
+        float average = (float)sum / (counter - 1);
+        int maxNumber = numbers.Max();
+
+        int minNumber = maxNumber;
+
+        foreach (int num in numbers)
+        {
+            if (num > 0 && num < minNumber)
+            {
+                minNumber = num;
+            }
+        }
+
+        numbers.Sort();
+
+        Console.WriteLine();
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maxNumber}");
+        Console.WriteLine($"The smallest number is: {minNumber}");
+        foreach (int n in numbers)
+        {
+            Console.WriteLine(n);
+        }
+
     }
 }
